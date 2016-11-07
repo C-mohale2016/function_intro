430b5deeaea348c4a85ca2798122b396232d67dd
C-mohale <clement.mohale@steltix.com>
2016-11-07 10:17:55 +0200

    initial commit

--- greet.js
+++ greet.js
@@ -1,9 +1,9 @@
 const assert = require('assert');
 var greet = function(name){
-  console.log('hi,'+ name)
+  console.log('hi,'+ name);
   return 'hi, '+ name;
 
 
 };
-greet('Jabu');
- assert.equal(greet('Jabu'), 'hi, Jabu');
+greet('Sibu');
+ assert.equal(greet('Sibu'), 'hi, Sibu');
