c127f8e0f98a039809241938eef6516ca6680792
C-mohale <clement.mohale@steltix.com>
2016-11-07 16:32:13 +0200

    initial commit

---
+++ countRegNumber.js
@@ -0,0 +1,8 @@
+function countRegNumber(registration) {
+  var regList = registration.split(',');
+  return regList.length;
+
+}
+const assert = require('assert');
+var regCount = countRegNumber ('CA 182736,CY 523519,CJ 812328');
+assert.equal(regCount, 3);
