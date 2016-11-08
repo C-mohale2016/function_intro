8bc87f8446f943a494aa9cfa34bade0ce9129826
C-mohale <clement.mohale@steltix.com>
2016-11-08 12:37:11 +0200

    initial commit

--- isTuesday.js
+++ isTuesday.js
@@ -1,6 +1,8 @@
 const assert = require('assert');
+// writing a function called isTuesday that returns a boolean Tuesday
 var isTuesday = function(day){
   Today = day.startsWith('Tu');
   return Today;
 };
 assert.equal(isTuesday('Tuesday'), true);
+assert.equal(isTuesday('Friday'), false);
